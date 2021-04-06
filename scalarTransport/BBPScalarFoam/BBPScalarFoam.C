60f7f73bf9440fe4930a1395b7ba0a8d8dc097d2
zhangzhihang <zhangzhihang@system76-pc.localdomain>
2021-04-06 17:29:33 -0400

    add rms output

--- scalarTransport/BBPScalarFoam/BBPScalarFoam.C
+++ scalarTransport/BBPScalarFoam/BBPScalarFoam.C
@@ -89,7 +89,7 @@ int main(int argc, char *argv[])
     #include "OFstream.H"
     fileName fname = ("total-C");
     OFstream os(fname);
-    os << "Time" << tab << "total-C" << tab << "std-C" << endl;
+    os << "Time" << tab << "total-C" << tab << "std" << tab << "rms" << endl;
 
     const scalarField& vol = mesh.V();
     scalar ttvol = gSum(vol);
@@ -132,11 +132,11 @@ int main(int argc, char *argv[])
         runTime.printExecutionTime(Info);
 
         scalar tC = fvc::domainIntegrate(C).value();
-        Info << "total C = " << tC << endl;
-        scalarField dC(C/one-tC/ttvol);
+        scalarField dC(C/one-tC/ttvol); //deviation of C
         scalar std = Foam::sqrt(gSum(vol*dC*dC)/ttvol);
-        Info << "std of C = " << std << endl;
-        os << runTime.timeName() << tab << tC << tab << std << endl;
+        scalarField vC(C/one); //value of C
+        scalar rms = Foam::sqrt(gSum(vol*vC*vC)/ttvol);
+        os << runTime.timeName() << tab << tC << tab << std << tab << rms << endl;
     }
 
     Info<< "End\n" << endl;
